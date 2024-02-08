5b274f0dcf08452db5d94185cc4634a79d4181ad
ayisha <ish.equal@gmail.com>
2024-02-08 08:31:18 -0400

    Hid all screens but allowed the intro screen to be shown when the page is loaded. Allowed the How to Play button to show the How to play screen when clicked.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+const howToPlaySec = document.getElementById('howToPlaySec');
+const homeSec = document.getElementById('intro');
+const howToPlayBtn = document.getElementById('howToPlayBtn');
 
+howToPlayBtn.addEventListener('click', navPageHow);
+
+// Hide All Screens
+function hideAllScreens() {
+    const sections = document.getElementsByTagName('section')
+    for (const section of sections) {
+        section.style.display = 'none'
+    }
+}
+
+// This Function Shows the How To Play Screen
+function navPageHow() {
+    hideAllScreens();
+    howToPlaySec.style.display = 'block';
+}
+
+
+function showIntro() {
+    hideAllScreens();
+    homeSec.style.display = 'block';
+}
+
+// This Function is called so that the Intro screen is displayed when the page loads
+showIntro();
